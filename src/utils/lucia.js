4e62007c5ce74c23cd5e380d21cfbc5067f9bc1b
akrajguru <akrajguru@gmail.com>
2024-06-21 15:19:46 -0700

    luciasdk updated

--- src/utils/lucia.js
+++ src/utils/lucia.js
@@ -1,10 +1,8 @@
-import Lucia from '../../node_modules/luciasdk-t1/lucia-sdk.js';
+import Lucia from 'luciasdk-t3';
 import 'dotenv/config';
 
 const lucia = new Lucia({
-  clientId: 'ondecentral',
-  //baseURL: 'http://localhost:3000',
-  baseURL: 'https://api.clickinsights.xyz',
+  baseURL: process.env.baseURL,
   api_key: process.env.API_KEY,
   //api_key: 'a256d8ef-992d41a6-2440e1a5-98f2853d-9d4fc59c-7b88f3b8-45692094-aee8489f',
 });
